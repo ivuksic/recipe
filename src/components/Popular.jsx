1ad4d73b585eecfbfe237068f64c113a363b6b56
Ivan Vuksic <ivuksic@gmail.com>
2022-10-30 01:08:53 +0200

    added slider and style

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,32 +1,56 @@
 import { useState, useEffect } from "react";
+import styled from 'styled-components'
+import {Splide, SplideSlide} from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
 
 function Popular() {
     const [popular,setPopular]= useState([])
 
-   useEffect(()=>{
+useEffect(()=>{
     getPopular()
-   },[])
+},[])
 
-   const getPopular = async ()=>{
+const getPopular = async ()=>{
     const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
     const data = await api.json()
     setPopular(data.recipes)
+    console.log(data.recipes);
     }
     
     return (
-    <div>
-        {popular.map((recipe)=>{
-            return(
-
-                <div key={recipe.id}>
-                <p>{recipe.title}</p>
-                </div>
+    <div>   
+        <Wrapper >
+            <h3>Popular Picks</h3>
+            <Splide>
+            {popular.map((recipe)=>{
+                return(
+                    <SplideSlide>
+                        <Card>
+                            <p>{recipe.title}</p>
+                            <img src={recipe.image} alt={recipe.title} />
+                        </Card>
+                    </SplideSlide>
                 )
-        })}
+            })}
+            </Splide>
+        </Wrapper>     
     </div>
     )
 }
     
+const Wrapper = styled.div`
+margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+min-height: 25rem;
+border-radius:2rem;
+overflow: hidden;
+
+img{
+    border-radius: 2rem;
+}
+`
 
 export default Popular
 
