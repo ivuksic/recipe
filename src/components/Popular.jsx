7a804b1a2caf8fb4624e46450268a77c28b764f5
Ivan Vuksic <ivuksic@gmail.com>
2022-10-30 01:29:32 +0200

    popular component finished

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -21,13 +21,20 @@ const getPopular = async ()=>{
     <div>   
         <Wrapper >
             <h3>Popular Picks</h3>
-            <Splide>
+            <Splide options={{
+                perPage: 4,
+                arrows: false,
+                pagination: false,
+                drag: 'free',
+                gap: '5rem'
+            }}>
             {popular.map((recipe)=>{
                 return(
                     <SplideSlide>
                         <Card>
                             <p>{recipe.title}</p>
                             <img src={recipe.image} alt={recipe.title} />
+                            <Gradient/>
                         </Card>
                     </SplideSlide>
                 )
@@ -43,14 +50,44 @@ margin: 4rem 0rem;
 `;
 
 const Card = styled.div`
-min-height: 25rem;
-border-radius:2rem;
-overflow: hidden;
+    min-height: 25rem;
+    border-radius:2rem;
+    overflow: hidden;
+    position: relative;
 
-img{
-    border-radius: 2rem;
-}
+    img{
+        border-radius: 2rem;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    p{
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, 0%);
+        color: white;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-item: center;
+    }
+`;
+
+const Gradient = styled.div`
+z-index: 3;
+position: absolute;
+width: 100%;
+height: 100%;
+background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
 `
+
 
 export default Popular
 
